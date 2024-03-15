dff047562f0358de8bad4c11a78aa528b458288d
tangrui <tangrui@si-tech.com.cn>
2024-03-16 00:29:11 +0800

    添加食材、部分转移食材功能开发

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private GameObject selectedCounter;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform topPoint;
+    [SerializeField] private bool testing = false;
+    [SerializeField] private ClearCounter transferTargetCounter;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetMouseButtonDown(0)) 
+        {
+            TransferKitchenObject(this,transferTargetCounter);
+        }
+    }
+
     public void Interact()
     {
-        print(this.gameObject + " Interacting ....");
+        // print(this.gameObject + " Interacting ....");
+        if (kitchenObject == null)
+        {
+            kitchenObject = GameObject.Instantiate(kitchenObjectSO.perfab, topPoint).GetComponent<KitchenObject>();
+            kitchenObject.transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            Debug.LogWarning("已经有了-" + gameObject.name);
+        }
     }
     public void SelectedCounter()
     {
@@ -16,5 +40,37 @@ public class ClearCounter : MonoBehaviour
     public void CancelSelected()
     {
         selectedCounter.SetActive(false);
+    }
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void TransferKitchenObject(ClearCounter sourceCounter, ClearCounter targetCounter)
+    {
+        if (sourceCounter.GetKitchenObject() == null)
+        {
+            Debug.LogWarning("源柜台上不存在食材，转移失败。");
+            return;
+        }
+        if (targetCounter.GetKitchenObject() != null)
+        {
+            Debug.LogWarning("目标柜台上存在食材，转移失败。");
+            return;
+        }
+        targetCounter.AddKitchenObject(sourceCounter.GetKitchenObject());
+        sourceCounter.ClearKitchenObject();
+
+    }
+    public void AddKitchenObject(KitchenObject kitchenObject)
+    {
+        kitchenObject.transform.SetParent(topPoint);
+        kitchenObject.transform.localPosition = Vector3.zero;
+        this.kitchenObject = kitchenObject;
+    }
+    // 清空原始食材
+    public void ClearKitchenObject()
+    {
+        this.kitchenObject = null;
     }
 }
---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class KitchenObjectSO : ScriptableObject
+{
+    public GameObject perfab;
+    public Sprite sprite;
+    public string objectName;
+
+}
